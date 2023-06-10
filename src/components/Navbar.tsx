9df73c24c46d9cfc76716c278dcba3fbeb442b84
Sandeepcr8 <sandeep@createprotocol.org>
2023-06-11 00:31:33 +0530

    fix: navbar size increase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,14 @@ import { useState } from "react";
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <div
-      className="left-1/2 flex -translate-x-1/2 justify-between px-4 py-4 md:absolute
-md:z-10 md:mt-5 md:w-[90%] md:rounded-md md:bg-[#161618] md:px-8 md:py-3 xl:px-10"
-    >
-      <Image src="/create-full-logo.svg" alt="logo" width={200} height={100} />
+    <div className="left-1/2 flex -translate-x-1/2 justify-between px-4 py-4 md:absolute md:z-10 md:mt-5 md:w-[90%] md:rounded-md md:bg-[#161618] md:px-8 md:py-3 xl:px-10 xl:py-4">
+      <Image
+        src="/create-full-logo.svg"
+        alt="logo"
+        width={200}
+        height={100}
+        className="lg:w-[14rem]"
+      />
       <Image
         src={navbarOpen ? "/close-icon.svg" : "/hamburger-icon.svg"}
         alt="logo"
@@ -17,9 +20,9 @@ md:z-10 md:mt-5 md:w-[90%] md:rounded-md md:bg-[#161618] md:px-8 md:py-3 xl:px-1
         className={`z-50 md:hidden ${navbarOpen ? "fixed right-5 top-5" : ""}`}
         onClick={() => setNavbarOpen(!navbarOpen)}
       />
-      <div className="hidden gap-8 md:flex">
+      <div className="hidden gap-8 md:flex md:items-center lg:gap-12">
         <a href="https://blog.createprotocol.org/" target="_blank">
-          <button className="text-sm text-white underline-offset-2 hover:underline">
+          <button className="text-sm text-white underline-offset-2 hover:underline lg:text-base">
             Blogs
           </button>
         </a>
@@ -27,7 +30,7 @@ md:z-10 md:mt-5 md:w-[90%] md:rounded-md md:bg-[#161618] md:px-8 md:py-3 xl:px-1
           href="https://create-protocol.gitbook.io/create-protocol-whitepaper/"
           target="_blank"
         >
-          <button className="rounded-full bg-white px-4 py-1.5 text-sm font-semibold text-black">
+          <button className="bg-button rounded-full px-4 py-1.5 text-sm font-semibold text-black lg:px-5 lg:py-2 lg:text-base">
             Whitepaper
           </button>
         </a>
