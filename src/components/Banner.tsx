345334635d11eebcc86c1d4c415e65429d032772
Sandeepcr8 <sandeep@createprotocol.org>
2023-08-11 15:09:19 +0530

    add: video on hero section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const text = "Outpace your Competition with Create Protocol";
+const text = "Outpace Your Competition With Create Protocol";
 
 const Banner = () => {
   const [title, setTitle] = useState("O");
@@ -15,31 +15,32 @@ const Banner = () => {
       } else {
         currentIndex++;
         setTitle((prevTitle) => prevTitle + text[currentIndex]);
-        console.log(currentIndex, text[currentIndex]);
       }
-    }, 250); // Adjust the typing speed as needed
+    }, 100); // Adjust the typing speed as needed
 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="relative h-[80vh] md:h-screen">
-      <img
-        src="/background.jpg"
-        alt="background"
+      <video
+        src="/background.mp4"
         width={100}
         height={100}
         className="h-[80vh] w-screen object-cover opacity-30 md:h-screen"
+        autoPlay
+        muted
+        loop
       />
-      <div className="absolute left-1/2 top-[40%] flex w-[90%] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-8 text-center sm:gap-8">
-        <div className="orange-line lg:w-[25rem]"></div>
-        <h2 className="text-4xl font-extrabold sm:text-4xl md:text-5xl lg:w-3/4 xl:w-3/5 xl:text-6xl">
+      <div className="absolute left-1/2 top-[50%] flex w-[90%] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-8 text-center sm:gap-8">
+        <div className="orange-line lg:invisible lg:w-[25rem]"></div>
+        <h2 className="text-3xl font-black sm:text-4xl md:w-3/5 lg:text-5xl 2xl:w-3/5 2xl:text-6xl min-[1700px]:text-7xl">
           <span className={`block animate-typing`}>{title}</span>
         </h2>
-        <p className="px-5 text-xl sm:w-3/4 md:w-1/2">
+        <p className="-mt-6 px-5 sm:w-3/4 lg:w-1/2 lg:text-lg xl:text-xl 2xl:text-2xl">
           We empower your dApps creation journey with unmatched speed &
           efficiency through the power of our Ecosystem and Decentralized AI
-          advancement.
+          advancement
         </p>
         <div className="orange-line lg:w-[25rem]"></div>
         <a
