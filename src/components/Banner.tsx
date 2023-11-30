cffa14154d7610f3497b1805b3e61b39f8d87dd1
Abhishek Krishna <invokerkrishna@gmail.com>
2023-11-30 23:19:08 +0530

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const text = "Outpace Your Competition With Create Protocol";
+const text = " Create With Create Protocol";
 
 const Banner = () => {
   const [title, setTitle] = useState("O");
